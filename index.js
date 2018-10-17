1950963714b2c80df073be8609d0e7480a51c306
MuathAlawi Alawi <muathalawi1@gmail.com>
2018-10-17 00:33:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ main.remove()
 const header = document.createElement('h1')
 header.id = 'victory'
 header.innerText = 'Nathan is the champion!'
-console.log(header)
 document.body.appendChild(header)
 const newHeader = document.querySelector('#victory');
 
