1679afba275b80af2b2f774d69e6bffad6451701
MuathAlawi Alawi <muathalawi1@gmail.com>
2018-10-17 00:35:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 /* Enter the code to remove the main node element under this comment */
 
+document.querySelector('#main').remove()
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+const header = document.createElement('h1')
+header.id = 'victory'
+header.innerText = 'Nathan is the champion!'
+document.body.appendChild(header)
+const newHeader = document.querySelector('#victory')
+
+
