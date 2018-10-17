a95c0debe3e3596b08ecfaa8a60416d45729e54b
MuathAlawi Alawi <muathalawi1@gmail.com>
2018-10-17 00:30:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 /* Enter the code to remove the main node element under this comment */
 
+document.querySelector('#main').remove()
 main.remove()
 /* Create your new element here and assign it to newHeader */
+
+const header = document.createElement('h1')
+header.id = 'victory'
+header.innerText = 'Nathan is the champion!'
+console.log(header)
+document.body.appendChild(header)
 const newHeader = null;
 
-document.createElement(h1)
+
