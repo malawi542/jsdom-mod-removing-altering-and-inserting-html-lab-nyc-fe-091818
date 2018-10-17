4a433b853662456f286483bab415a8fb0ef6c25a
MuathAlawi Alawi <muathalawi1@gmail.com>
2018-10-17 00:34:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* Enter the code to remove the main node element under this comment */
 
 document.querySelector('#main').remove()
-main.remove()
+
 /* Create your new element here and assign it to newHeader */
 
 const header = document.createElement('h1')
